f040cedce0747ae08d31d37aa7eb8ed2b72374a8
Sachin Sourabh <Sachin.Sourabh@MSBC-MAINFRAME.LCL>
2022-11-23 19:23:13 +0530

    Updates

---
+++ precache-manifest.8903e31c8ec25f291f392eb514f27dd7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799b3069390e00de217232951a17572a",
+    "url": "/FireBaseFlipkart-React/index.html"
+  },
+  {
+    "revision": "6f734ad6368e67d58d45",
+    "url": "/FireBaseFlipkart-React/static/css/2.d4dfd184.chunk.css"
+  },
+  {
+    "revision": "30dfcb15947001d92a05",
+    "url": "/FireBaseFlipkart-React/static/css/main.c176f300.chunk.css"
+  },
+  {
+    "revision": "6f734ad6368e67d58d45",
+    "url": "/FireBaseFlipkart-React/static/js/2.4ab48c5b.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/FireBaseFlipkart-React/static/js/2.4ab48c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30dfcb15947001d92a05",
+    "url": "/FireBaseFlipkart-React/static/js/main.4a30925d.chunk.js"
+  },
+  {
+    "revision": "3ef0f914dd993a409a11",
+    "url": "/FireBaseFlipkart-React/static/js/runtime-main.83a89af2.js"
+  },
+  {
+    "revision": "67ebd076b506a82802783f022b812983",
+    "url": "/FireBaseFlipkart-React/static/media/1.67ebd076.jpg"
+  },
+  {
+    "revision": "d6cdaa5d771ee0b6001b9a416a2cfb00",
+    "url": "/FireBaseFlipkart-React/static/media/10.d6cdaa5d.png"
+  },
+  {
+    "revision": "70673495ac88b358837f833b5d79017f",
+    "url": "/FireBaseFlipkart-React/static/media/2.70673495.png"
+  },
+  {
+    "revision": "5efddf54f6bba868056ca2aefdf4f6df",
+    "url": "/FireBaseFlipkart-React/static/media/3.5efddf54.png"
+  },
+  {
+    "revision": "3200642c0c4efe04538d49532198efa4",
+    "url": "/FireBaseFlipkart-React/static/media/4.3200642c.png"
+  },
+  {
+    "revision": "f8c7acd4b5aeea3632c1c0f47713c135",
+    "url": "/FireBaseFlipkart-React/static/media/5.f8c7acd4.png"
+  },
+  {
+    "revision": "44d3808d6ac0929eb76916fd5ac04ae0",
+    "url": "/FireBaseFlipkart-React/static/media/52.44d3808d.png"
+  },
+  {
+    "revision": "1e84ba67f19ab7ebb2f28412e2b9e9d0",
+    "url": "/FireBaseFlipkart-React/static/media/53.1e84ba67.png"
+  },
+  {
+    "revision": "ceaeb8654f2e5e4f4456518b01b2338a",
+    "url": "/FireBaseFlipkart-React/static/media/54.ceaeb865.png"
+  },
+  {
+    "revision": "419cc2c8cbca2429d5a0ac9275883a13",
+    "url": "/FireBaseFlipkart-React/static/media/6.419cc2c8.png"
+  },
+  {
+    "revision": "6f7cda37a90b578e3def395ca8ede70b",
+    "url": "/FireBaseFlipkart-React/static/media/7.6f7cda37.png"
+  },
+  {
+    "revision": "433a789474704e3f8043e9ba5ce49782",
+    "url": "/FireBaseFlipkart-React/static/media/8.433a7894.png"
+  }
+]);
